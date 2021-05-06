d3dbdc892cf8bfc9f1092dfcb0efac436da2481b
Sledgerobby35 <sledge.robby35@gmail.com>
2021-05-06 18:23:07 -0500

    add let and const challenges

---
+++ 10-Days-of-Javascript/js/Day1-letAndConst.js
@@ -0,0 +1,13 @@
+(function(){
+	"use strict";
+	function main(){
+		const PI = Math.PI;
+		let r = readLine().;
+		if(0 < r < 100){
+			console.log(PI * (Math.pow(r, 2)));
+			console.log(2 * (PI * r));
+		} else {
+			console.log(false);
+		}
+	}
+})();
