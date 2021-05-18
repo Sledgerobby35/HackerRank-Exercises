ac177f2e5fbd6ff28722734918910dfecfb2012a
Sledgerobby35 <sledge.robby35@gmail.com>
2021-05-18 16:08:23 -0500

    add Day4 Classes exercise

---
+++ 10-Days-of-Javascript/js/Day4-classes.js
@@ -0,0 +1,12 @@
+(function(){
+	"use strict"
+	class Polygon {
+		constructor(height, width) {
+			this.height = height;
+			this.width = width;
+		}
+	}
+	let arr = [1,2,3,4]
+	let c = new Polygon(arr);
+	console.log(c);
+})();
